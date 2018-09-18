55ded07791008d10bda07578a482dc8593ef27a9
Antoine Bache <antoine.bache@epitech.eu>
2018-09-18 22:18:28 +0200

    Added OS tweaks

---
+++ tweak_os.sh
@@ -0,0 +1,181 @@
+#!/usr/bin/env bash
+
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+function ok() {
+    echo -e "$COL_GREEN[ok]$COL_RESET "$1""
+}
+
+function running() {
+    echo -ne "$COL_YELLOW ⇒ $COL_RESET"$1": "
+}
+
+running "cleanup homebrew"
+brew cleanup > /dev/null 2>&1
+ok
+
+running "closing any system preferences to prevent issues with automated changes"
+osascript -e 'tell application "System Preferences" to quit'
+ok
+
+running "Disable remote login"
+sudo systemsetup -setremotelogin off
+
+running "Disable wake-on modem"
+sudo systemsetup -setwakeonmodem off
+
+running "Disable wake-on LAN"
+sudo systemsetup -setwakeonnetworkaccess off
+
+running "Disable guest account login"
+sudo defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool false
+
+running "Disable local Time Machine snapshots"
+sudo tmutil disablelocal;ok
+
+running "Remove the sleep image file to save disk space"
+sudo rm -rf /Private/var/vm/sleepimage;ok
+running "Create a zero-byte file instead"
+sudo touch /Private/var/vm/sleepimage;ok
+running "…and make sure it can’t be rewritten"
+sudo chflags uchg /Private/var/vm/sleepimage;ok
+
+running "Increase window resize speed for Cocoa applications"
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001;ok
+
+running "Save to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false;ok
+
+running "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true;ok
+
+running "Restart automatically if the computer freezes"
+sudo systemsetup -setrestartfreeze on;ok
+
+running "Never go into computer sleep mode"
+sudo systemsetup -setcomputersleep Off > /dev/null;ok
+
+running "Allow quitting via ⌘ + Q; doing so will also hide desktop icons"
+defaults write com.apple.finder QuitMenuItem -bool true;ok
+
+running "Show hidden files by default"
+defaults write com.apple.finder AppleShowAllFiles -bool true;ok
+
+running "Show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true;ok
+
+running "Show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true;ok
+
+running "Show path bar"
+defaults write com.apple.finder ShowPathbar -bool true;ok
+
+running "Avoid creating .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true;ok
+
+running "Empty Trash securely by default"
+defaults write com.apple.finder EmptyTrashSecurely -bool true;ok
+
+running "Enable AirDrop over Ethernet and on unsupported Macs running Lion"
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true;ok
+
+running "Show the ~/Library folder"
+chflags nohidden ~/Library;ok
+
+running "Expand the following File Info panes: “General”, “Open with”, and “Sharing & Permissions”"
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+  General -bool true \
+  OpenWith -bool true \
+  Privileges -bool true;ok
+
+running "Disable Dashboard"
+defaults write com.apple.dashboard mcx-disabled -bool true;ok
+
+running "Don’t show Dashboard as a Space"
+defaults write com.apple.dock dashboard-in-overlay -bool true;ok
+
+running "Don’t automatically rearrange Spaces based on most recent use"
+defaults write com.apple.dock mru-spaces -bool false;ok
+
+running "Set Safari’s home page to ‘about:blank’ for faster loading"
+defaults write com.apple.Safari HomePage -string "about:blank";ok
+
+running "Prevent Safari from opening ‘safe’ files automatically after downloading"
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;ok
+
+running "Disable Safari’s thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2;ok
+
+running "Enable Safari’s debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true;ok
+
+running "Make Safari’s search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false;ok
+
+running "Remove useless icons from Safari’s bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()";ok
+
+running "Enable the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true;ok
+
+running "Add a context menu item for showing the Web Inspector in web views"
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true;ok
+
+running "Disable Spotlight indexing for any volume that gets mounted and has not yet been indexed"
+# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes";ok
+running "Change indexing order and disable some file types from being indexed"
+defaults write com.apple.spotlight orderedItems -array \
+  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+  '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+  '{"enabled" = 1;"name" = "PDF";}' \
+  '{"enabled" = 1;"name" = "FONTS";}' \
+  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+  '{"enabled" = 0;"name" = "MESSAGES";}' \
+  '{"enabled" = 0;"name" = "CONTACT";}' \
+  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+  '{"enabled" = 0;"name" = "IMAGES";}' \
+  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+  '{"enabled" = 0;"name" = "MUSIC";}' \
+  '{"enabled" = 0;"name" = "MOVIES";}' \
+  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+  '{"enabled" = 0;"name" = "SOURCE";}';ok
+running "Load new settings before rebuilding the index"
+killall mds > /dev/null 2>&1;ok
+running "Make sure indexing is enabled for the main volume"
+sudo mdutil -i on / > /dev/null;ok
+
+running "Prevent Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true;ok
+
+running "Disable local Time Machine backups"
+hash tmutil &> /dev/null && sudo tmutil disablelocal;ok
+
+running "Show the main window when launching Activity Monitor"
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true;ok
+
+running "Visualize CPU usage in the Activity Monitor Dock icon"
+defaults write com.apple.ActivityMonitor IconType -int 5;ok
+
+running "Show all processes in Activity Monitor"
+defaults write com.apple.ActivityMonitor ShowCategory -int 0;ok
+
+running "Sort Activity Monitor results by CPU usage"
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0;ok
+
+running "Enable the debug menu in Disk Utility"
+defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+defaults write com.apple.DiskUtility advanced-image-options -bool true;ok
