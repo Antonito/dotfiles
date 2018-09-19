1b3353f03275a4e49230a477a74310d1c1822e35
Antoine Bache <antoine.bache@epitech.eu>
2018-09-19 10:32:04 +0200

    Added gitconfig, improved OS tweaks, automated MacOS software installation and configuration

---
+++ install_apps.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+source .private_script_helpers/colors.sh
+source .private_script_helpers/require.sh
+
+# ---------------
+# Brew
+# ---------------
+bot "Installing Homebrew"
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew update
+#brew cask upgrade
+RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl` --with-readline-dir=`brew --prefix readline` --with-libyaml-dir=`brew --prefix libyaml`"
+require_brew ruby
+
+# ---------------
+# Dependencies
+# ---------------
+bot "Installing dependencies"
+require_brew git
+require_brew mas
+require_brew fontconfig
+
+# ---------------
+# Communication
+# ---------------
+bot "Installing communication tools"
+require_cask slack
+require_cask telegram
+require_cask skype-for-business
+require_cask caprine
+
+# ---------------
+# Mail
+# ---------------
+bot "Installing communication tools"
+require_mas "Spark" "1176895641"
+
+# ---------------
+# Media
+# ---------------
+bot "Installing Media"
+require_cask spotify
+
+# ---------------
+# TextEditor
+# ---------------
+bot "Installing Text Editors"
+require_mas "Xcode" "497799835"
+require_brew neovim
+require_cask visual-studio-code
+require_cask atom
+
+# ---------------
+# Desktop Utility
+# ---------------
+bot "Installing Desktop Utilities"
+require_mas "Keynote" "409183694"
+require_mas "Numbers" "409203825"
+require_mas "Pages" "409201541"
+
+# ---------------
+# Utility
+# ---------------
+bot "Installing Utilities"
+require_brew git-flow
+require_brew fish
+require_cask iterm2
+require_cask google-chrome
+require_cask dashlane
+require_cask boom-3d
+require_cask daisydisk
+
+# ---------------
+# Virtualization
+# ---------------
+bot "Installing Virtualization tools"
+require_cask docker
+
+# ---------------
+# Configuration
+# ---------------
+
+# Set Fish as default shell
+CURRENTSHELL=$(dscl . -read /Users/$USER UserShell | awk '{print $2}')
+if [[ "$CURRENTSHELL" != "/usr/local/bin/fish" ]]; then
+  bot "setting newer homebrew fish (/usr/local/bin/fish) as your shell (password required)"
+  # sudo bash -c 'echo "/usr/local/bin/fish" >> /etc/shells'
+  # chsh -s /usr/local/bin/fish
+  sudo dscl . -change /Users/$USER UserShell $SHELL /usr/local/bin/fish > /dev/null 2>&1
+  ok
+fi
+
+bot "Installing fonts"
+./fonts/install.sh
+brew tap caskroom/fonts
+require_cask font-fontawesome
+require_cask font-awesome-terminal-fonts
+require_cask font-hack
+require_cask font-inconsolata-dz-for-powerline
+require_cask font-inconsolata-g-for-powerline
+require_cask font-inconsolata-for-powerline
+require_cask font-roboto-mono
+require_cask font-roboto-mono-for-powerline
+require_cask font-source-code-pro
+ok
+
+if [[ -d "/Library/Ruby/Gems/2.0.0" ]]; then
+  running "Fixing Ruby Gems Directory Permissions\n"
+  sudo chown -R $(whoami) /Library/Ruby/Gems/2.0.0
+  ok
+fi
+
+running "Cleaning-up homebrew\n"
+brew cleanup > /dev/null 2>&1
+ok
--- tweak_os.sh
+++ tweak_os.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-# Colors
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_YELLOW=$ESC_SEQ"33;01m"
-COL_BLUE=$ESC_SEQ"34;01m"
-COL_MAGENTA=$ESC_SEQ"35;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
-
-function ok() {
-    echo -e "$COL_GREEN[ok]$COL_RESET "$1""
-}
-
-function running() {
-    echo -ne "$COL_YELLOW ⇒ $COL_RESET"$1": "
-}
+source .private_script_helpers/colors.sh
 
 running "cleanup homebrew"
 brew cleanup > /dev/null 2>&1
