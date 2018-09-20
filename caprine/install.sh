5b6e47746e28812d43bf48680eb98742868e59cd
Antoine Bache <antoine.bache@epitech.eu>
2018-09-20 15:53:45 +0200

    Fixed caprine typo

--- caprine/install.sh
+++ caprine/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdkr -p "$HOME/Library/Application Support/Caprine/"
+mkdir -p "$HOME/Library/Application Support/Caprine/"
 cp ./config.json "$HOME/Library/Application Support/Caprine/config.json"
