1f9ea3e41b13574e1b03596bf3164a7ad01413fe
Antoine Bache <antoine.bache@epitech.eu>
2018-09-22 15:39:49 +0200

    Added bartender configuration

--- macos/configure_apps.sh
+++ macos/configure_apps.sh
@@ -169,6 +169,11 @@ function setup_bettertouchtool() {
   cd bettertouchtool; ./install.sh; cd -;
 }
 
+function setup_bartender() {
+  substep "Configuring Bartender"
+  cd bartender; ./install.sh; cd -;
+}
+
 function setup_dash() {
   substep "Configuring Dash"
   cd dash; ./install.sh; cd -;
@@ -186,6 +191,7 @@ set_fish_default_shell
 setup_ruby
 setup_fish
 setup_bettertouchtool
+setup_bartender
 setup_dash
 setup_skype_for_business
 setup_git
