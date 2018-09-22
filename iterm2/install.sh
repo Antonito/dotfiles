794064b8cefc89a51e6b2c05fd5713c6195fd348
Antoine Bache <antoine.bache@epitech.eu>
2018-09-22 17:28:46 +0200

    Improved iTerm2 configuration

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -8,3 +8,19 @@ cp com.googlecode.iterm2.plist $HOME/.config/iterm2/
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+# Hide Pane titles
+defaults write com.googlecode.iterm2.plist ShowPaneTitles -bool false
+# Set Dark mode
+defaults write com.googlecode.iterm2.plist TabStyle -int 1
+# Hide Tab bar in fullscreen mode
+defaults write com.googlecode.iterm2.plist ShowFullScreenTabBar -bool false
+# Hide window number
+defaults write com.googlecode.iterm2.plist WindowNumber -bool false
+# Hide job name
+defaults write com.googlecode.iterm2.plist JobName -bool false
+# Hide tab close button
+defaults write com.googlecode.iterm2.plist HideTabCloseButton -bool true
+# Hide Activity indicator
+defaults write com.googlecode.iterm2.plist HideActivityIndicator -bool true
+# Don't flash tab bar when switching tabs in fullscreen mode
+defaults write com.googlecode.iterm2.plist FlashTabBarInFullscreen -bool false
